ab20461b50e4dbc6ac2b1c62ebf111c5b1b2eac1
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2025-03-11 10:54:06 +0000

    opa

---
+++ encapsulamento/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+    //nome, email e senha
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+
+    }
+    
+}
