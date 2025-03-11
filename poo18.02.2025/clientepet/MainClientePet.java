9c90a2fffd5d2653c85a9b05fdc95f09c528b034
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2025-03-11 11:00:57 +0000

    save2

--- poo18.02.2025/clientepet/MainClientePet.java
+++
@@ -1,8 +0,0 @@
-public class MainClientePet {
-    public static void main(String[] args) {
-        Pet pet1 = new Pet("desgraca", "3", "husky");
-        PetCliente petCliente = new PetCliente("Caio", "18", pet1);
-        System.out.println(petCliente.toString());
-
-    }
-}
